ad04a28889a1496a0d457534258962624dd7b15d
ritikgangwarr <ritik.gangwar638rg@gmail.com>
2023-12-07 11:13:27 +0530

    FIRST COMMIT

---
+++ resources/scripts/rps.js
@@ -0,0 +1,24 @@
+let userMove='';
+computerMove='';
+console.log(computerMove);
+let result='';
+
+function generate_Computer_move(){
+    const C = ["rock", "paper", "scissors"];
+    let ch=(Math.floor(Math.random() * 10)%3);
+    computerMove= C[ch];
+}
+
+function evaluateMoves(){
+    if(userMove === computerMove){
+        result='tie';
+    }
+    else if((userMove==='rock' && computerMove==='scissors')||(userMove==='paper' && computerMove==='rock')||(userMove==='scissor' && computerMove==='paper')){
+        result='win';
+    }
+    else
+    {
+        result='lose';
+    }
+
+}
